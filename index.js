cf9f346902d04affeef6a7f5d9e4dcaf90916824
Cristian Antir <antircristian@gmail.com>
2024-11-11 22:46:35 +0100

    fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.info("DOM loaded.");
-    const user = {
+    const entity = {
         distance: {
-            yearly: null,
+            year: null,
+        },
+        fuel: {
+            type: null,
+            efficiency: null,
+            efficiencyUnit: null,
+            unit: null,
         },
     };
 
@@ -30,23 +36,112 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
+    //Distance
     const distance = document.getElementById("distance");
     if (distance) {
-        distance.addEventListener("input", ({ target }) => {
-            target.value = target.value.replace(",", ".");
-        });
-
         distance.addEventListener("keydown", (event) => {
             checkKeydownData(event);
         });
 
+        distance.addEventListener("input", ({ target }) => {
+            target.value = target.value.replace(",", ".");
+        });
+
         distance.addEventListener("change", ({ target }) => {
-            let input = parseFloat(target.value);
+            let input = parseInt(target.value);
             if (isNaN(input)) {
                 input = 0;
             }
-            user.distance.yearly = input;
-            target.value = parseFloat(input).toFixed(0);
+
+            target.value = input;
+            entity.distance.year = input;
         });
+
+        let initialValue = parseInt(distance.value);
+        if (isNaN(initialValue)) {
+            return;
+        }
+        entity.distance.year = initialValue;
     }
+
+    //Fuel units
+    const fuels = document.querySelectorAll('input[name="fuelType"]');
+    const fuelUnits = document.querySelectorAll(".fuelUnit");
+
+    const setFuelUnit = () => {
+        if (entity.fuel.type === "electric") {
+            entity.fuel.unit = "kWh";
+        } else {
+            entity.fuel.unit = "L";
+        }
+    };
+
+    const handleFuelUnits = (fuel) => {
+        entity.fuel.type = fuel.value;
+        setFuelUnit();
+        for (const el of fuelUnits) {
+            el.innerText = entity.fuel.unit;
+        }
+    };
+
+    for (const fuel of fuels) {
+        fuel.addEventListener("change", () => {
+            handleFuelUnits(fuel);
+        });
+
+        if (fuel.checked) {
+            handleFuelUnits(fuel);
+        }
+    }
+
+    //Efficiency
+    const efficiency = document.getElementById("efficiency");
+    if (efficiency) {
+        efficiency.addEventListener("keydown", (event) => {
+            checkKeydownData(event);
+        });
+
+        efficiency.addEventListener("input", ({ target }) => {
+            target.value = target.value.replace(",", ".");
+        });
+
+        efficiency.addEventListener("change", ({ target }) => {
+            const input = parseFloat(parseFloat(target.value).toFixed(1));
+            if (isNaN(input)) {
+                input = 0;
+            }
+
+            target.value = input;
+            entity.fuel.efficiency = input;
+            console.log("input", input);
+        });
+
+        const initialValue = parseFloat(
+            parseFloat(efficiency.value).toFixed(1),
+        );
+
+        if (isNaN(initialValue)) {
+            return;
+        }
+        entity.fuel.efficiency = initialValue;
+    }
+
+    const efficiencyUnits = document.querySelectorAll(
+        'input[name="fuelEfficiency"]',
+    );
+    if (efficiencyUnits) {
+        for (const type of efficiencyUnits) {
+            type.addEventListener("change", () => {
+                if (type.checked) {
+                    entity.fuel.efficiencyUnit = type.value;
+                }
+            });
+
+            if (type.checked) {
+                entity.fuel.efficiencyUnit = type.value;
+            }
+        }
+    }
+
+    console.log("entity:", entity);
 });
